c92a23612ee6ce1a22d635b6433f43e8145660cc
RoiMP <roimartinezportela@gmail.com>
2023-03-06 20:38:01 +0100

    Added dock minigame
    Use this minigame's view and template as an example to add yuor
    own minigame

--- src/minigames/urls.py
+++ src/minigames/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import minigame_view
+from .views import mock_minigame_view
 
 urlpatterns = [
-    path('minigame/', minigame_view),
+    path('mock_minigame/', mock_minigame_view),
     # path('tu-minijuego/', tu_minijuego_view),
 ]
--- src/minigames/views.py
+++ src/minigames/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 
-def minigame_view(request):
+def mock_minigame_view(request):
     """Default Minigame View
     TODO: remove
     """
-    template_name = 'minigame.html'
+    template_name = 'mock_minigame/mock_minigame.html'
     return render(request, template_name)
