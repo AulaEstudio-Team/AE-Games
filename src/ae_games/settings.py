9eb99bfa2aaab97aa6ae2f24b4fe7adac64c2af2
RoiMP <roimartinezportela@gmail.com>
2023-03-06 20:30:26 +0100

    Added new apps and static files

--- src/ae_games/settings.py
+++ src/ae_games/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kb)k%287czvzv%dt@sh5@93!9)v!g_)xg0w1thr)b7^xc_by1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Libraries
+    'bootstrap5',
+
+    # Custom apps
+    'minigames.apps.MinigamesConfig',
+    'social.apps.SocialConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,9 @@ ROOT_URLCONF = 'ae_games.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
