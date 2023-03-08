9206efd622f184ad46d5379c0d7b2ef58939cbdb
carlosgamallo <carlosgamallez@gmail.com>
2023-03-08 20:54:59 +0100

    cambios

--- src/minigames/urls.py
+++ src/minigames/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import mock_minigame_view
+from .views import mock_minigame_view, my_guerradelperujuego
 
 urlpatterns = [
     path('mock_minigame/', mock_minigame_view),
+    path('guerra_peru', my_guerradelperujuego),
     # path('tu-minijuego/', tu_minijuego_view),
 ]
--- src/minigames/views.py
+++ src/minigames/views.py
@@ -7,3 +7,10 @@ def mock_minigame_view(request):
     """
     template_name = 'mock_minigame/mock_minigame.html'
     return render(request, template_name)
+
+def my_guerradelperujuego(request):
+    """Guerra del peru minigame
+    TODO: remove
+    """
+    template_name = 'guerra_peru\index.html'
+    return render(request, template_name)
