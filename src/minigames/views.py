7ce12dfd552663052599b1b0d64e424483d7489a
alexgallegov <alexgalve10@gmail.com>
2023-03-08 20:46:05 +0100

    cambios prueba

--- src/minigames/views.py
+++ src/minigames/views.py
@@ -7,3 +7,9 @@ def mock_minigame_view(request):
     """
     template_name = 'mock_minigame/mock_minigame.html'
     return render(request, template_name)
+
+def ahorcado_view(request):
+    """Default Minigame View
+    """
+    template_name = 'minijuego_ahorcado/ahorcado.html'
+    return render(request, template_name)
