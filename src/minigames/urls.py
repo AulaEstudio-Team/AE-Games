b75bec4819002146f02daf5eab49fa68ae949bac
COSTRAS67 <ericcostras@gmail.com>
2023-03-08 20:39:29 +0100

    Creada pagina para tron

--- src/minigames/urls.py
+++ src/minigames/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import mock_minigame_view
+from .views import mock_minigame_view,TRON_Legacy_minigame_view
 
 urlpatterns = [
     path('mock_minigame/', mock_minigame_view),
     # path('tu-minijuego/', tu_minijuego_view),
+    path('tron_legacy/', TRON_Legacy_minigame_view),
 ]
--- src/minigames/views.py
+++ src/minigames/views.py
@@ -7,3 +7,9 @@ def mock_minigame_view(request):
     """
     template_name = 'mock_minigame/mock_minigame.html'
     return render(request, template_name)
+
+def TRON_Legacy_minigame_view(request):
+    """ view of TRON LEGACY
+    """
+    template_name = 'tron_legacy/tron.html'
+    return render(request, template_name)
