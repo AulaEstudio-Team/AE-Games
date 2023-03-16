81d2a13a9e15f1309e5c947dce9f0be63e31cffa
Carlos Soto Alonso <csotalo@aulaestudio.es>
2023-03-16 18:33:52 +0100

    Update

--- src/social/views.py
+++ src/social/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+
 
 
 def home_view(request):
     """Home View"""
     template_name = 'home.html'
     return render(request, template_name)
+
+def register_view(request):
+    """Register view"""
+    template_name = "register.html"
+    return render(request, template_name)
+
+# class registerUser(CreateView):
+    # model = User
+    # template_name = "register.html"
+    # from_class = UserCreationForm
+    # success_url = "home.html"
