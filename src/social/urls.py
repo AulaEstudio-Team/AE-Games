17790ce3e4e2d9f2b8b1e80a91f8bd47a9be8861
IvanPeM <ivan.pe.martinez@gmail.com>
2023-03-16 19:58:40 +0100

    intento de recogida de datos

--- src/social/urls.py
+++ src/social/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import home_view
+from .views import profile_view
 
 urlpatterns = [
     path('', home_view),
     # path('', log_in),
-    # path('', profile),
+    path('profile/', profile_view),
     # path('', global_laderboards),
 ]
--- src/social/views.py
+++ src/social/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from social.models import UnModelo
 
 def home_view(request):
     """Home View"""
     template_name = 'home.html'
     return render(request, template_name)
+
+def profile_view(request):
+    """Profile View"""
+    mis_datos= UnModelo.objects.all().values()
+    context = {'datos_lista': mis_datos}
+    template_name = 'profile/profile.html'
+    return render(request, template_name,context)
