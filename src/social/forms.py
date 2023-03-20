af9581c39b476f8ea8ff59ebc81646a4de504f89
Carlos Soto Alonso <csotalo@aulaestudio.es>
2023-03-20 20:13:17 +0100

    Forms Creation

--- src/social/forms.py
+++ src/social/forms.py
@@ -1,15 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from social.models import Profile
+from django.shortcuts import render, redirect
 
 # Sign Up Form
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+class Model1Form(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'user'
+        ]
 
+        user = forms.CharField(max_length=30, required=False)
+
+
+class Model2Form(UserCreationForm):
     class Meta:
         model = User
+        exclude = ('Profile_one_to_one_field',)
         fields = [
             'username', 
             'first_name', 
@@ -18,3 +28,28 @@ class SignUpForm(UserCreationForm):
             'password1', 
             'password2', 
             ]
+
+
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254)
+    username = forms.CharField(max_length=30, required=False, help_text="")
+    password1 = forms.CharField(help_text="")
+
+def create_models(request):
+    if request.method == 'POST':
+        form1 = Model1Form(request.POST)
+        form2 = Model2Form(request.POST)
+        if all([form1.is_valid(), form2.is_valid()]):
+            model1 = form1.save()
+            model2 = form2.save(commit=False)
+            model2.User_1_one_to_one_field = model1
+            model2.save()
+            return redirect('create_models_success')
+        else:
+            form1 = Model1Form()
+            form2 = Model2Form()
+            return render(request, 'register.html',{'form1' : form1, 'form2': form2})
+
+    # class CommentForm(forms.Form):
+        # comment = forms.CharField(widget=forms.Textarea(), label=None)
--- src/social/urls.py
+++ src/social/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import home_view, RegisterView, profile_view, login
 
 urlpatterns = [
-    path('', home_view),
+    path('', home_view, name="home"),
     path('register/', RegisterView.as_view()),
-    path('login/', login),
+    path('login/', login, name="login"),
     path('profile/', profile_view)
     # path('', log_in),
     # path('', global_laderboards),
