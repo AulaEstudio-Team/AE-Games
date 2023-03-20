73ebcd0fd8459e508b1f76de34025f46c74588b2
carlosgamallo <carlosgamallez@gmail.com>
2023-03-20 20:30:45 +0100

    merge del main

--- src/minigames/urls.py
+++ src/minigames/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import mock_minigame_view, conecta4, wordle_minigame, TRON_Legacy_minigame_view, ahorcado_view, my_guerradelperujuego, atari_breakout
+from .views import mock_minigame_view, conecta4, wordle_minigame, TRON_Legacy_minigame_view, ahorcado_view, tic_tac_toe, atari_breakout
 
 urlpatterns = [
     path('mock_minigame/', mock_minigame_view),
-    path('guerra_peru', my_guerradelperujuego),
+    path('TicTacToe/', tic_tac_toe),
     path('wordle/', wordle_minigame),
     # path('tu-minijuego/', tu_minijuego_view),
     path('ahorcadogame/', ahorcado_view),
