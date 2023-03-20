bde81fc4955ff49595d2ce0615c499517abcb470
alexgallegov <alexgalve10@gmail.com>
2023-03-20 20:11:15 +0100

    Correccion error en ruta imagenes del script

--- src/minigames/static/ahorcado/script.js
+++ src/minigames/static/ahorcado/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_6.png';
+document.getElementById("imagen2").src='img/ahorcado_6.png';
 
 const LISTA=["gato", "hotel", "subasta", "onomatopeya", "esternocleidomastoideo","desoxirribonucleico", "casa", "jeringuilla", "ladron", "bistec", "ranura", "equipo", "menhir"]
 
@@ -78,22 +78,22 @@ function verificar(palabra_random, array, fallos){
         }
     }
     if(fallos==1){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_5.png';
+        document.getElementById("imagen2").src='img/ahorcado_5.png';
     }
     if(fallos==2){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_4.png';
+        document.getElementById("imagen2").src='img/ahorcado_4.png';
     }
     if(fallos==3){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_3.png';
+        document.getElementById("imagen2").src='img/ahorcado_3.png';
     }
     if(fallos==4){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_2.png';
+        document.getElementById("imagen2").src='img/ahorcado_2.png';
     }
     if(fallos==5){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_1.png';
+        document.getElementById("imagen2").src='img/ahorcado_1.png';
     }
     if(fallos==6){
-        document.getElementById("imagen2").src='AE-Games\src\minigames\static\ahorcado\img\ahorcado_1.png';
+        document.getElementById("imagen2").src='img/ahorcado_1.png';
         alert("Has perdido, pulsa F5 para jugar de nuevo");
         
     }
@@ -102,4 +102,4 @@ function verificar(palabra_random, array, fallos){
     }
 }
 
-onload(mostrar_texto.innerHTML=array)
+window.onload(mostrar_texto.innerHTML=array);
